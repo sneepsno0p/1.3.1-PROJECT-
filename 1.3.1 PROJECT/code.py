baeb8f499229dc6e907b21e642045bbc82bd657d
sneepsno0p <91640771+sneepsno0p@users.noreply.github.com>
2021-12-01 11:13:09 -0800

    Add files via upload

---
+++ 1.3.1 PROJECT/code.py
@@ -0,0 +1,13 @@
+import turtle as trtl
+sus = trtl.Turtle()
+cj = trtl.Turtle()
+spag = trtl.Turtle()
+lvp = trtl.Turtle()
+mc = trtl.Turtle()
+lef = trtl.Turtle()
+
+epic = {"ds":"e"}
+print(epic["ds"])
+wn = trtl.Screen()
+wn.bgpic("NAWS2.gif")
+wn.mainloop()
